28343b2aa5cd40f91b3cbecb11e718d20a9e8cbd
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-25 16:18:17 +0530

    Added default constructor in book entity and added getBook endpoint

--- src/main/java/poc/orm/HibernatePOC/controller/BookAPI.java
+++ src/main/java/poc/orm/HibernatePOC/controller/BookAPI.java
@@ -1,18 +1,27 @@
 package poc.orm.HibernatePOC.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import poc.orm.HibernatePOC.entity.Book;
 import poc.orm.HibernatePOC.response.BookResponse;
 
+import java.util.Arrays;
+
 @RestController
 public class BookAPI {
 
-    @GetMapping("/books/all")
+    @GetMapping("/books")
     public ResponseEntity<BookResponse> getAllBook() {
         BookResponse bookResponse = new BookResponse();
+        return ResponseEntity.ok(bookResponse);
+    }
+
+    @GetMapping("/books/{bookId}")
+    public ResponseEntity<BookResponse> getBook(@PathVariable("bookId") Integer bookId) {
+        BookResponse bookResponse = new BookResponse();
+        bookResponse.setBooks(Arrays.asList(new Book()));
         return ResponseEntity.ok(bookResponse);
     }
 }
--- src/main/java/poc/orm/HibernatePOC/entity/Book.java
+++ src/main/java/poc/orm/HibernatePOC/entity/Book.java
@@ -12,6 +12,10 @@ public class Book {
     private Long id;
     private String name;
 
+    public Book() {
+
+    }
+
     public Book(Long id, String name) {
         this.id = id;
         this.name = name;
