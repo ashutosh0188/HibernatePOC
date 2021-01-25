1a56385d031581432e247058ac7edfc5eee2f5cf
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-25 15:06:17 +0530

    Added basic springboot application with springdatajpa dependency

---
+++ src/main/java/poc/orm/HibernatePOC/HibernatePocApplication.java
@@ -0,0 +1,13 @@
+package poc.orm.HibernatePOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/orm/HibernatePOC/HibernatePocApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.orm.HibernatePOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
